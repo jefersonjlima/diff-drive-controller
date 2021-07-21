98e8d75e82c656e2ed9f9355adfae464e078ddba
Jeferson Lima <jefersonjl82@gmail.com>
2021-07-21 00:05:33 -0300

    Add Robot control and Gazebo Service control

--- include/diff_drive_controller/pid_control.hpp
+++ include/diff_drive_controller/pid_control.hpp
@@ -1,15 +1,20 @@
 #ifndef PID_CONTROL_HPP
 #define PID_CONTROL_HPP
+#define DEBUG
 
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Twist.h>
 #include <tf/transform_datatypes.h>
-
+#include <std_srvs/Empty.h>
+#include <gazebo_msgs/SpawnModel.h>
+#include <fstream>
 
 typedef struct
 {
-  double p, i, d;
-  double x_max_vel, z_max_vel;
+  double p_x, i_x, d_x;
+  double p_yaw, i_yaw, d_yaw;
+  double x_max_vel, yaw_max_vel;
 } PID;
 
 typedef struct
@@ -26,26 +31,30 @@ class PID_Control{
     PID_Control(ros::NodeHandle node, ros::NodeHandle private_nh);
     ~PID_Control(){ _node.shutdown();};
 
-     double calculateTargetDistance(const Orientation& pose, const Orientation& target);
-     double linearControl(const Orientation& pose);
-     double angularControl(const Orientation& pose);
+    void resetGazebo();
+    double calculateTargetDistance(const Orientation& pose, const Orientation& target);
+    double linearControl(const Orientation& pose, const Orientation& target);
+    double angularControl(const Orientation& pose, const Orientation& target);
+    void moveTarget();
   private:
-    void init();
+    void initGoal();
 
     /*---ROS Topics-------------------*/
-    ros::Publisher cmd_pub;
-    ros::Subscriber odom_sub;
+    ros::Publisher _cmd_pub;
+    ros::Subscriber _odom_sub;
     ros::NodeHandle _node;
+    ros::ServiceClient _spawn;
 
     void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
 
     /*---Variables--------------------*/
-
     std::string _cmd_vel_publisher;
     std::string _odometry_subscriber;
+    gazebo_msgs::SpawnModel _visual_target;
     PID _pid;
     Orientation _pose;
     Orientation _target;
+    bool _move;
 
 };
 
